d6e26951b223e8e01a9c6344c1ed40d9f5ee3c31
Vazgen-Bablumyan <vazgenBablumyan7@gmail.com>
2022-03-02 14:30:15 +0400

    added catch block

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let weather = {
     "appKey": "8a8b3d206c1144641fbe45db04a1e712",
     fetchWeather: function (city) {
+
         fetch("https://api.openweathermap.org/data/2.5/weather?q="
             + city
             + "&lon={lon}&appid="
@@ -8,16 +9,20 @@ let weather = {
         )
             .then((response) => response.json())
             .then((data) => this.displayWeather(data))
+            .catch((error) => alert("a city called " + city + " not found"))
+
+
 
     },
     displayWeather: function (data) {
-       
+        console.log(data)
         const { name, sys } = data;
         const { icon, description } = data.weather[0];
         const { temp, humidity } = data.main;
         const { speed } = data.wind;
 
-        document.querySelector(".city").innerText = "Weather in " + name + '\n\n' + "Country code " + sys.country;
+        document.querySelector(".city").innerText = "Weather in " + name;
+        document.querySelector(".country-code").innerText = "Country code " + sys.country;
         document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + "@2x.png";
         document.querySelector(".description").innerText = description;
         document.querySelector(".temp").innerText = temp + "°C";
@@ -25,6 +30,8 @@ let weather = {
         document.querySelector(".wind").innerText = "Wind speed: " + speed + " km/h";
         document.querySelector(".weather").classList.remove("loading");
         document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?" + name + "')"
+
+
     },
 
     search: function () {
